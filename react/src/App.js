7c71d1a301e223d2d229f4888391d905630e8a02
kandonie <kandonie@wpi.edu>
2020-02-25 16:22:47 -0500

    started profile page

--- react/src/App.js
+++ react/src/App.js
@@ -3,16 +3,19 @@ import  {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
 import Navbar from './components/Navbar'
 import Map from './components/Map'
+import Profile from './components/Profile'
 
 export default () => (
   <Router>
     <Navbar>
+    <Profile>
     <Switch>
     <Route path="/">
       <Map />
     </Route>
 
     </Switch>
+    </Profile>
     </Navbar>
   </Router>
 
---
+++ react/src/components/Profile.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Profile extends React.Component {
+    render() {
+        return <h1>Kristen Andonie {this.props.name}</h1>;
+    }
+}
