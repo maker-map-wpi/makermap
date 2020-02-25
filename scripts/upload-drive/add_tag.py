d3ece8b177e9945643cf248aa282a17d6bba123f
Arjun Gandhi <arjungandhi06@gmail.com>
2020-02-25 09:43:45 -0500

    wrote script to push google sheets to db

---
+++ scripts/upload-drive/add_tag.py
@@ -0,0 +1,20 @@
+import mysql.connector
+import json
+import uuid
+
+# connect to db
+mydb = mysql.connector.connect(
+  host="makermap.cbeezzrvvyp6.us-east-2.rds.amazonaws.com",
+  user="admin",
+  passwd="makermap",
+  database="innodb"
+)
+
+cursor = mydb.cursor()
+
+
+def addTag(type, tagID, tag):
+    sql_cmd = ("INSERT INTO Labs(idTags, TaggedObjTable, TaggedObjID, Tag) VALUES (%s, %s, %s, %s)")
+    data = (str(uuid.uuid4()), type, tagID, tag)
+    cursor.execute(sql_cmd, data)
+    mydb.commit()
